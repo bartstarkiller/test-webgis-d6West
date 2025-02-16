74160cd28bfb6ba79575b7951eb0dc6e16514564
bart <186526608+bartstarkiller@users.noreply.github.com>
2025-02-16 17:16:42 -0600

    Add files via upload
    vile evil map updates (reprehensible)

--- js/qgis2web_expressions.js
+++ js/qgis2web_expressions.js
@@ -757,7 +757,7 @@ function fnc_project_color(values, context) {
 
 
 
-function exp_Planets_2rule0_eval_expression(context) {
+function exp_Planets_4rule0_eval_expression(context) {
     // canon = 1
 
     var feature = context.feature;
@@ -770,7 +770,7 @@ function exp_Planets_2rule0_eval_expression(context) {
 }
 
 
-function exp_Planets_2rule1_eval_expression(context) {
+function exp_Planets_4rule1_eval_expression(context) {
     // canon = 0
 
     var feature = context.feature;
